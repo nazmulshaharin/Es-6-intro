d073870fe17e5ce8a738a883f7e9f71cf0c46ca1
nazmulshaharin <smnazmulhossainshaharin@gmail.com>
2022-08-16 21:38:28 +0600

    first commit

---
+++ 1-basic.js
@@ -0,0 +1,20 @@
+//variable
+var deposit = 500;
+//condition 
+if(deposit > 400){
+
+}
+
+// array
+const numbers = [22, 25, 28, 15, 20, 35];
+const numberCount = numbers.length;
+numbers.push(111);
+numbers.pop();
+numbers[2] = 555;
+// included in the array
+if(numbers.indexOf(222) != -1){
+
+}
+if(numbers.includes(222)){
+  
+}

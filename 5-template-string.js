a080d4ca285a8a4a75310a0d5577d1eac66f4010
nazmulshaharin <smnazmulhossainshaharin@gmail.com>
2022-08-24 11:27:38 +0600

    dbnsbdshehhydeh

--- 5-template-string.js
+++ 5-template-string.js
@@ -7,5 +7,20 @@ const multiLine = 'this is my first line. \n' +
 const newMultiLine = `this is my first line.
 this is my first line.
 this is my first line.`
-console.log(multiLine);
-console.log(newMultiLine);
+
+const friends = ['babul', 'kabul', 'sabul'];
+const friendCount = 5;
+const old = '<h3 class="friend-name">friend-3</h3>'
+const newSystem = `<h3 class="friend-name">friend-${friendCount}</h3>`;
+const newSystem2 = `<h3 class="friend-name">friend-${friends.length}</h3>`
+
+//
+const first = 'mamun';
+const last = 'Hasan';
+const fullNew = `This person name is: ${first} ${last}. Has money ${friends.length * 500}. He lives in Dhaka.`;
+console.log(fullNew);
+
+console.log(newSystem);
+console.log(newSystem2);
+//console.log(multiLine);
+//console.log(newMultiLine);

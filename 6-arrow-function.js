5a0e876c5dcb8afaedc2950291766708132a03f5
nazmulshaharin <smnazmulhossainshaharin@gmail.com>
2022-08-24 13:45:29 +0600

    djdjdj

---
+++ 6-arrow-function.js
@@ -0,0 +1,16 @@
+function add(num1, num2){
+  return num1 + num2;
+}
+console.log(add(5, 3));
+// function expression(anonymous)
+
+const add2 = function(num1, num2){
+  return num1 + num2;
+}
+const sum2 = add2(16, 20);
+
+//arrow function
+const add3 = (num1, num2) => num1 + num2;
+const sum3 = add3(40, 20);
+//
+console.log(sum2, sum3);;

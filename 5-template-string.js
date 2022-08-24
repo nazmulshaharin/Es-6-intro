3bd75709ea09454238e934ef2b574bdb58725fcc
nazmulshaharin <smnazmulhossainshaharin@gmail.com>
2022-08-24 11:01:22 +0600

    dhhdhdhdhh

---
+++ 5-template-string.js
@@ -0,0 +1,11 @@
+const priya = 'Asif Akbar';
+const meye = 'meye tomi ki dokkho cino';
+const kobita = `tmi ki amar moner kobita`;
+const multiLine = 'this is my first line. \n' +
+'this is my second line. \n' +
+'this is my third line';
+const newMultiLine = `this is my first line.
+this is my first line.
+this is my first line.`
+console.log(multiLine);
+console.log(newMultiLine);
